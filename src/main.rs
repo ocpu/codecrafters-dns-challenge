46631c8bb24760597873ee7c518751091a4deea7
ocpu <martin.hovre@opencubes.io>
2023-11-21 14:42:21 +0900

    Add tracing and debug view

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ mod question;
 mod resource;
 mod types;
 
+use tracing::Level;
+
 use crate::{
     domain_name::DomainName,
     header::{Opcode, ResponseCode},
@@ -21,7 +23,7 @@ use std::{
 const MAX_MESSAGE_SIZE: usize = 512;
 
 fn main() {
-    println!("Logs from your program will appear here!");
+    tracing_subscriber::fmt().with_max_level(Level::TRACE).init();
     let mut map = HashMap::new();
     map.insert(
         DomainName::from_static("codecrafters.io"),
@@ -37,11 +39,15 @@ fn main() {
             eprintln!("ERROR: receiving data from socket");
             break;
         };
-        println!("Input: {:?}", &buf[..size]);
+        let span = tracing::trace_span!("dns_request", source = %source);
+        let _guard = span.enter();
+        if cfg!(debug_assertions) {
+            print_buffer("Input", &buf[..size]);
+        }
         let packet = match DNSPacket::try_parse(&buf[..size]) {
             Ok(packet) => packet,
             Err(e) => {
-                eprintln!("Failed to parse packet: {e:?}");
+                tracing::error!(error = "Failed to parse packet", message = ?e);
                 let (_, resp_size) = DNSPacket::try_parse_header_only(&buf[..size])
                     .unwrap_or_else(|| DNSPacket::new(0))
                     .respond(ResponseCode::FormatError)
@@ -62,7 +68,7 @@ fn main() {
         });
 
         for q in packet.questions() {
-            println!("Question: {q}");
+            tracing::trace!(section = "question", domain_name = %q.name(), r#type = ?q.q_type(), class = ?q.q_class());
             builder =
                 builder.add_question(Question::new(*q.q_type(), *q.q_class(), q.name().clone()));
             builder = builder.add_answer(
@@ -78,9 +84,53 @@ fn main() {
             .build_into(&mut response[..])
             .expect("TODO: split response?");
 
-        println!("Output: {:?}", &response[..resp_size]);
+        if cfg!(debug_assertions) {
+            print_buffer("Output", &response[..resp_size]);
+        }
         udp_socket
             .send_to(&response[..resp_size], source)
             .expect("Failed to send response");
     }
+}
+
+#[cfg(debug_assertions)]
+const LINE_ITEM_COUNT: usize = 16;
+
+#[cfg(debug_assertions)]
+fn print_buffer(label: &str, mut buffer: &[u8]) {
+    use nom::AsChar;
+
+    eprintln!("--- Begin {label} ---");
+    loop {
+        let slice = &buffer[..usize::min(LINE_ITEM_COUNT, buffer.len())];
+        buffer = &buffer[slice.len()..];
+
+        for i in 0..LINE_ITEM_COUNT {
+            if let Some(byte) = slice.get(i) {
+                if *byte < 16 {
+                    eprint!("0");
+                }
+                eprint!("{byte:x?} ");
+            } else {
+                eprint!("   ");
+            }
+        }
+
+        for i in 0..LINE_ITEM_COUNT {
+            if let Some(byte) = slice.get(i) {
+                if byte.is_ascii_alphanumeric() || *byte == b'-' {
+                    eprint!("{}", byte.as_char());
+                } else {
+                    eprint!(".");
+                }
+            } else {
+                eprint!(" ");
+            }
+        }
+        eprint!("\n");
+        if buffer.is_empty() {
+            break;
+        }
+    }
+    eprintln!("--- End {label} ---");
 }
